8f96dae105d1039cd88f11d71204a990391added
Kingsley Motsepe <kmotsepe@gmail.com>
2016-12-23 23:26:03 +0200

    base project

---
+++ src/test/java/za/co/kmotsepe/epimethius/test/TestDummy.java
@@ -0,0 +1,19 @@
+package za.co.kmotsepe.epimethius.test;
+
+import static org.testng.Assert.assertTrue;
+import org.testng.annotations.Test;
+
+/**
+ * 
+ * @author Kingsley Motsepe
+ * Dummy unit test
+ */
+public class TestDummy {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testTrue(){
+        assertTrue( true );
+    }
+}
